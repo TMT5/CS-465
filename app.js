eb90b03256b9f0c065b6d4c709127a9b9229306c
Terius Thweatt <teriusthweatt@Teriuss-MacBook-Air.local>
2026-05-21 11:59:43 -0400

    Module 2 Completed

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+var indexRouter = require('./app_server/routes/index');
+var travelRouter = require('./app_server/routes/travel');
+var handlebars = require('hbs');
 const express = require('express');
 const path = require('path');
+
+handlebars.registerPartials(path.join(__dirname, 'app_server/views/partials'));
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
