6c538db9717f3fc6d63bb2db300be696c9e957b9
Terius Thweatt <teriusthweatt@Teriuss-MacBook-Air.local>
2026-05-27 12:19:06 -0400

    Module 3 completed

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ handlebars.registerPartials(path.join(__dirname, 'app_server/views/partials'));
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'app_server/views'));
+app.set('view engine', 'hbs');
+app.get('/travel.html', (req, res) => {
+  res.redirect('/travel');
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/travel', travelRouter);
